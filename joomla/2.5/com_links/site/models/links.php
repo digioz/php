d1d71935d766dc4e8642d25c28da7fa244c56a7c
digioz <digioz@gmail.com>
2014-04-18 10:28:58 -0500

    Disabled the pagingation for the public links list view by setting 'list.limit' to 0.

--- joomla/2.5/com_links/site/models/links.php
+++ joomla/2.5/com_links/site/models/links.php
@@ -41,18 +41,20 @@ class LinksModelLinks extends JModelList {
 
         // List state information
         $limit = $app->getUserStateFromRequest('global.list.limit', 'limit', $app->getCfg('list_limit'));
-        $this->setState('list.limit', $limit);
+		$this->setState('list.limit', 0);
+		
+        // $this->setState('list.limit', $limit);
 
-        $limitstart = JFactory::getApplication()->input->getInt('limitstart', 0);
-        $this->setState('list.start', $limitstart);
+        //$limitstart = JFactory::getApplication()->input->getInt('limitstart', 0);
+        //$this->setState('list.start', $limitstart);
 
         
-		if(empty($ordering)) {
-			$ordering = 'a.ordering';
-		}
+		//if(empty($ordering)) {
+		//	$ordering = 'a.ordering';
+		//}
 
         // List state information.
-        parent::populateState($ordering, $direction);
+        //parent::populateState($ordering, $direction);
     }
 
     /**
