5902a8c4a40652ab579e92f6262d7153b3283574
digioz <digioz@gmail.com>
2016-01-08 07:48:49 -0600

    Added top navigation and changed the boxes to all be blue.

--- joomla/3.0/templates/index.php
+++ joomla/3.0/templates/index.php
@@ -20,22 +20,50 @@ $doc->addScript('templates/' . $this->template . '/js/main.js', 'text/javascript
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
+	<!-- Top Navigation -->
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+		  <div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+			  <span class="sr-only">Toggle navigation</span>
+			  <span class="icon-bar"></span>
+			  <span class="icon-bar"></span>
+			  <span class="icon-bar"></span>
+			</button>
+		  </div>
+		  <div class="navbar-collapse collapse">
+			<jdoc:include type="modules" name="position-1" style="none" />
+		  </div><!--/.nav-collapse -->
+		</div>
+	</nav>
+
     <!-- main container -->
-    <div class='container'>
+    <div class="container-fluid" style="padding:10px;">
         <!-- header -->
         <div class='row'>
-				<jdoc:include type="modules" name="header" style="none" />
-				<jdoc:include type="modules" name="position-1" style="none" />
-				<jdoc:include type="modules" name="position-3" style="xhtml" />
-				<jdoc:include type="modules" name="position-2" style="none" />
+			<jdoc:include type="modules" name="header" style="none" />
+			
+			<jdoc:include type="modules" name="position-3" style="xhtml" />
+			<jdoc:include type="modules" name="position-2" style="none" />
+        </div>
+		
+		<p>&nbsp;</p>
+		
+		<!-- banner -->
+		<?php if ($this->countModules('banner')) : ?>
+        <div class='row'>
+			<jdoc:include type="modules" name="banner" style="none" />
         </div>
 		<p>&nbsp;</p>
-        <!-- mid container - includes main content area and right sidebar -->
+		
+		<?php endif; ?>
+		
+        <!-- mid container - includes main content and left and right navs -->
         <div class='row'>
 			<!-- left sidebar -->
 			<?php if ($this->countModules('position-4')) : ?>
-			<div class='col-sm-3'>
-				<div class="panel panel-default">
+			<div class="col-sm-2">
+				<div class="panel panel-primary">
 					<div class="panel-heading">
 						<h3 class="panel-title">&nbsp;</h3>
 					</div>
@@ -47,8 +75,8 @@ $doc->addScript('templates/' . $this->template . '/js/main.js', 'text/javascript
 			<?php endif; ?>
 			
             <!-- main content area -->
-            <div class='col-sm-6'>
-				<div class="panel panel-default">
+            <div class="col-sm-8">
+				<div class="panel panel-primary">
 					<div class="panel-heading">
 						<h3 class="panel-title">&nbsp;</h3>
 					</div>
@@ -61,8 +89,8 @@ $doc->addScript('templates/' . $this->template . '/js/main.js', 'text/javascript
 			
             <!-- search box -->
 			<?php if ($this->countModules('position-0')) : ?>
-			<div class='col-sm-3'>
-				<div class="panel panel-default">
+			<div class="col-sm-2">
+				<div class="panel panel-primary">
 					<div class="panel-heading">
 						<h3 class="panel-title">&nbsp;</h3>
 					</div>
@@ -75,8 +103,8 @@ $doc->addScript('templates/' . $this->template . '/js/main.js', 'text/javascript
 			
 			<!-- right sidebar -->
 			<?php if ($this->countModules('position-7')) : ?>
-            <div class='col-sm-3'>
-				<div class="panel panel-default">
+            <div class="col-sm-2">
+				<div class="panel panel-primary">
 					<div class="panel-heading">
 						<h3 class="panel-title">&nbsp;</h3>
 					</div>
