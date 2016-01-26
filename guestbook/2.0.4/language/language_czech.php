cd3b1b18d271c57f5c6b8b76228ab9b7d96b0b15
digioz <digioz@gmail.com>
2016-01-26 07:15:51 -0600

    Added Czech Language File to Version 2.0.4.

---
+++ guestbook/2.0.4/language/language_czech.php
@@ -0,0 +1,43 @@
+<?php
+
+$ Yournametxt = "Vaše jméno:";
+$ Youremailtxt = "Váš e-mail:";
+$ YourMessagetxt = "Vaše zpráva:";
+$ Submitbutton = "Odeslat";
+$ Headingtitletxt = "Kniha návštěv";
+$ Addentrytxt = "Přidat záznam";
+$ Viewguestbooktxt = "Zobrazit Knihu návštěv";
+$ Newpostfirsttxt = "Nový příspěvek jako první";
+$ Newpostlasttxt = "Nový příspěvek jako poslední";
+$ Addentryheadtxt = "Kniha návštěv - Výsledek vstupu:";
+$ Error1 = "Zadaný název je příliš dlouhý.";
+$ Error2 = "Zadaný Email je příliš dlouhý.";
+$ Error3 = "Vadná emailová adresa.";
+$ Error4 = "Prázdné pole: <b> Vaše jméno </ b>";
+$ Error5 = "Prázdné pole: <b> Váš e-mail </ b>";
+$ Error6 = "Prázdné pole: <b> Vaše zpráva </ b>";
+$ Error7 = "Váš příspěvek do Knihy návštěv nemohl být zpracován v tomto okamžiku.";
+$ Error8 = "Zkuste to znovu o několik minut.";
+$ GoBack = "Klikněte zde pro návrat";
+$ Result1 = "Následující zpráva byla přidána do knihy návštěv";
+$ Result2 = "Zpráva byla zapsána.";
+$ Listnametxt = "Od";
+$ Listemailtxt = "Email";
+$ ListMessagetxt = "Zpráva";
+$ ListDatetxt = "Datum";
+$ Searchlabeltxt = "Zadejte hledané slovo:";
+$ Searchbuttontxt = "Najít!";
+$ ErrorImageVerification = "Špatně odepsán ověřovací kód. <br /> Prosím, vraťte se zpět a zadejte ho znovu!";
+$ Msgfloodprotection = "Je nám líto, ale už jste zaslali zprávu do této Knihy návštěv <br> Prosím, vyčkejte 2 minuty a zkuste to znovu.";
+$ Msgreferrerkey = "Pokoušíte se předložit tuto položku z <br> NEOPRÁVNĚNÉHO UMÍSTĚNÍ. Vaše IP adresa byla zaznamenána. <br> Varujeme Vás, že pokud budete pokračovat v pokusech <br> opakovaného zápisu do této návštěvní knihy, budou vyvozeny <br> právní kroky proti Vám a Vaší organizaci. ";
+$ Msgspamdetected = "detekován SPAM! <br> Vaše IP adresa byla zaznamenána <br> a bude oznámena Vašemu poskytovateli internetových služeb.";
+$ Title = "DigiOz Kniha";
+$ Msgnonnumericpagenr = "Ne numerické číslo strany";
+$ Msgsortingerror = "Vstupní údaj může být pouze 'asc' nebo 'desc'";
+$ Msgnoentries = "Momentálně zde nejsou žádné položky k zobrazení.";
+$ Msgnosearchterm = "Prosím, zadejte hledaný výraz a zkuste to znovu.";
+$ Msgresultofsearch = "Výsledek vyhledávání";
+$ Msgnomatchfound = "Pro hledaný výraz nebyla nalezena žádná shoda.";
+$ ErrorFormToken = "Byl zjištěn neplatný formát ochranného klíče.";
+
+?>
